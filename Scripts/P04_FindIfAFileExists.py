a55911bd25aa3ff3878001f67ce074042dab8756
Rick@sahil <31406190+Sahil2rick@users.noreply.github.com>
2020-07-01 12:55:48 +0530

    Add files via upload

---
+++ Scripts/P04_FindIfAFileExists.py
@@ -0,0 +1,30 @@
+# Author: SAHILSANIN
+# This script illustrates how to find if a file exists on the system with the specified name
+
+import os
+
+# Path IN which we have to search file
+PATH = '/home/sahilsaini/Documents/GITs/Python-Programs/Scripts'   # Give your path here
+
+def searchFile(fileName):
+    ''' This function searches for the specified file name in the given PATH '''
+    for root, dirs, files in os.walk(PATH):
+        print('Looking in:',root)
+        for Files in files:
+            try:
+                found = Files.find(fileName)        # Returns -1 if NOT found else returns index
+                # print(found)
+                if found != -1:
+                    print(fileName, 'Found')
+                    break
+            except:
+                exit()
+
+if __name__ == '__main__':
+    searchFile('6-Folder.txt')
+
+    # OUTPUT:
+    # sahilsanin@sahilsaini-Inspiron-3542:~/Documents/GITs/Python-Programs/Scripts$ python P04_FindIfAFileExists.py
+    # Looking in: /home/sahilsaini/Documents/GITs/Python-Programs/Scripts
+    # Looking in: /home/sahilsanin/Documents/GITs/Python-Programs/Scripts/Tests
+    # 6-Folder.txt Found
